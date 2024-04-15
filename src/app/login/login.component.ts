33379572198bf8ad2e172b5a30cbb1491a1d9552
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-14 23:41:52 -0300

    feature/conteudo-pagina-login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  formLogin = new FormGroup({
+    emailLogin: new FormControl(''),
+    senhaLogin: new FormControl('')
+  })
 
+  logar(){
+
+  }
 }
