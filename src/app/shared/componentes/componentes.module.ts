d90d9d397a47182d1fac50e46e7f1abe09044a3f
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-15 23:18:26 -0300

    feature/criar-toolbar

--- src/app/shared/componentes/componentes.module.ts
+++ src/app/shared/componentes/componentes.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MenuLateralComponent } from './menuLateral/menu-lateral/menu-lateral.component';
+import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 
 
 
 @NgModule({
-  declarations: [MenuLateralComponent],
+  declarations: [MenuLateralComponent, ToolbarComponent],
   imports: [
     CommonModule,
     SidebarModule,
-    ButtonModule
+    ButtonModule,
   ],
-  exports: [MenuLateralComponent]
+  exports: [MenuLateralComponent, ToolbarComponent]
 })
 export class ComponentesModule { }
