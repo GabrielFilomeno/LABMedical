026cbd711ea408a13fe1df2922d9e865195f7433
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-18 13:01:16 -0300

    feature/conteudo-pag-cadastro-paciente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ComponentesModule } from './shared/componentes/componentes.module';
+import { CadastroPacienteComponent } from "./cadastro-paciente/cadastro-paciente.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [ComponentesModule],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    imports: [ComponentesModule, CadastroPacienteComponent]
 })
 export class AppComponent {
   title = 'LABMedical';
--- src/app/cadastro-paciente/cadastro-paciente.component.ts
+++ src/app/cadastro-paciente/cadastro-paciente.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectItem } from 'primeng/api';
+import { CalendarModule } from 'primeng/calendar';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-cadastro-paciente',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ReactiveFormsModule, DropdownModule, CalendarModule, InputMaskModule, InputNumberModule],
   templateUrl: './cadastro-paciente.component.html',
   styleUrl: './cadastro-paciente.component.scss'
 })
 export class CadastroPacienteComponent {
+  formCadastroPaciente = new FormGroup({
+    nomePaciente: new FormControl(''),
+    genero: new FormControl(''),
+    dataNascimento: new FormControl<Date | null>(null),
+    cpf: new FormControl(''),
+    rg: new FormControl(''),
+    estadoCivil: new FormControl(''),
+    telefone: new FormControl(''),
+    email: new FormControl(''),
+    naturalidade: new FormControl(''),
+    convenio: new FormControl(''),
+    numeroCarteira: new FormControl(''),
+    validade: new FormControl<Date | null>(null),
+    cep: new FormControl(''),
+    cidade: new FormControl(''),
+    estado: new FormControl(''),
+    logradouro: new FormControl(''),
+    numero: new FormControl(''),
+    complemento: new FormControl(''),
+    bairro: new FormControl(''),
+    pontoReferencia: new FormControl('')
+  })
+  
+  generos: SelectItem[];
+  estadosCivis: SelectItem[];
 
+  constructor() {
+    this.generos = [
+      {label:'Feminino', value:'Feminino'},
+      {label:'Masculino', value:'Masculino'},
+      {label:'Não-binário', value:'Não-binário'}
+    ];
+    this.estadosCivis = [
+      {label:'Solteiro', value:'Solteiro'},
+      {label:'Casado', value:'Sasado'},
+      {label:'Divorciado', value:'Divorciado'},
+      {label: 'Viúvo', value: 'Viúvo'}
+    ];
+  }
+
+  cadastrar () {};
 }
