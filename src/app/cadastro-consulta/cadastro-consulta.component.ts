5c1b2dc558a1a5d123adde4a900f7d9628ea0b94
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-18 13:11:52 -0300

    feature/criar-pag-cadastrar-consulta

---
+++ src/app/cadastro-consulta/cadastro-consulta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-consulta',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-consulta.component.html',
+  styleUrl: './cadastro-consulta.component.scss'
+})
+export class CadastroConsultaComponent {
+
+}
