409110b3710fdff9ad12ef0c7e7e60c78644b8f5
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-18 13:49:46 -0300

    feature/conteudo-pag-cadastrar-consulta

--- src/app/cadastro-consulta/cadastro-consulta.component.ts
+++ src/app/cadastro-consulta/cadastro-consulta.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'app-cadastro-consulta',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, InputTextModule, ButtonModule, ReactiveFormsModule, CalendarModule, InputTextareaModule],
   templateUrl: './cadastro-consulta.component.html',
   styleUrl: './cadastro-consulta.component.scss'
 })
 export class CadastroConsultaComponent {
+  formCadastroPaciente = new FormGroup({
+    nomePaciente: new FormControl(''),
+    dataConsulta: new FormControl<Date | null>(null),
+    horaConsulta: new FormControl<Date | null>(null),
+    descProblema: new FormControl(''),
+    medicacaoReceitada: new FormControl(''),
+    dosagensPrecaucoes: new FormControl('')
+  });
+  procurarPaciente: any;
+
+cadastrar(){};
 
 }
