e0844ccade150b91eb58c50c6cafb112a178ced9
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-18 13:59:43 -0300

    feature/criar-pag-cadastrar-exame

---
+++ src/app/cadastro-exame/cadastro-exame.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-exame',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-exame.component.html',
+  styleUrl: './cadastro-exame.component.scss'
+})
+export class CadastroExameComponent {
+
+}
