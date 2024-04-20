52ef7209bc7457dc2cf51cb359f9ff614e12903c
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-20 20:10:59 -0300

    feature/conteudo-pag-listar-prontuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { ComponentesModule } from './shared/componentes/componentes.module';
-import { CadastroConsultaComponent } from "./cadastro-consulta/cadastro-consulta.component";
-import { CadastroExameComponent } from "./cadastro-exame/cadastro-exame.component";
+import { ListarProntuariosComponent } from "./listar-prontuarios/listar-prontuarios.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [ComponentesModule, CadastroExameComponent]
+    imports: [ComponentesModule, ListarProntuariosComponent]
 })
 export class AppComponent {
   title = 'LABMedical';
