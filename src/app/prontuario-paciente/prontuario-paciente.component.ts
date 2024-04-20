8caced98150576547a2a4ba3bcecab4323cf1d1c
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-20 20:20:11 -0300

    feature/criar-pag-prontuario-paciente

---
+++ src/app/prontuario-paciente/prontuario-paciente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prontuario-paciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './prontuario-paciente.component.html',
+  styleUrl: './prontuario-paciente.component.scss'
+})
+export class ProntuarioPacienteComponent {
+
+}
