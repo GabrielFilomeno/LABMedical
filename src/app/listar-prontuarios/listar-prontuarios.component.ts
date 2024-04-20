09d958a2b16bdf4ec0ae8b81c1dcfdb60cd39c61
GabrielFilomeno <biel_floripa@hotmail.com>
2024-04-20 19:32:15 -0300

    feature/criar-pag-listar-prontuarios

---
+++ src/app/listar-prontuarios/listar-prontuarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-prontuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-prontuarios.component.html',
+  styleUrl: './listar-prontuarios.component.scss'
+})
+export class ListarProntuariosComponent {
+
+}
